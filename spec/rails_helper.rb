fdb517378a0bc6a086737f0c8fee282bfe3deb6f
Ricky Amparo <rickyamparo@gmail.com>
2018-02-27 16:42:12 -0700

    create a request helper that parses json response

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'database_cleaner'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+Dir[Rails.root.join('spec/helpers/request_spec_helper.rb')].each { |f| require f }
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -24,6 +25,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  config.include RequestSpecHelper, type: :request
 
   config.include FactoryBot::Syntax::Methods
 
