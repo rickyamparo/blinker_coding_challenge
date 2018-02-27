af91a6ccdcbedbc0e5147029dd0ae03f48e80c4e
Ricky Amparo <rickyamparo@gmail.com>
2018-02-27 13:36:55 -0700

    fix table name in create vehicle migration

--- db/migrate/20180227202607_create_vehicles.rb
+++ db/migrate/20180227202607_create_vehicles.rb
@@ -1,6 +1,6 @@
 class CreateVehicles < ActiveRecord::Migration[5.1]
   def change
-    create_table :create_vehicles do |t|
+    create_table :vehicles do |t|
       t.string :make
       t.string :model
       t.string :options, array: true, default: []
