54f17269dc429df1329c0a4518f4d9afa97e29e4
Ricky Amparo <rickyamparo@gmail.com>
2018-02-27 14:07:15 -0700

    create seeds file using random fake car data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Vehicle.destroy_all
+
+FAKE_MAKE = ["hyundai", "subaru", "volkswagen", "toyota", "ford"]
+FAKE_MODEL = ["smart", "2-door", "4-door", "truck", "self-driving"]
+FAKE_OPTIONS = ["air conditioning", "cup holders", "spinning rims", "aux port", "hybrid"]
+
+puts "Creating Vehicles"
+10.times do
+  options_array = FAKE_OPTIONS.sample(rand(0..4))
+
+  Vehicle.create(
+    make: FAKE_MAKE[rand(0..4)],
+    model: FAKE_MODEL[rand(0..4)],
+    options: options_array
+  )
+end
+
+puts "Finish creating Vehicles"
