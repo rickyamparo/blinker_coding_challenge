f054cbc890d4f93cc47fda23ad0600db4da4aa0f
Ricky Amparo <rickyamparo@gmail.com>
2018-02-27 16:53:29 -0700

    create vehcile show action spec

--- spec/requests/vehicles_spec.rb
+++ spec/requests/vehicles_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Vehicles API", type: :request do
   let!(:vehicles) {create_list(:vehicle, 10)}
+  let(:vehicle_id) { vehicles.first.id }
 
   describe 'GET /api/v1/vehicles' do
     before { get '/api/v1/vehicles' }
@@ -13,6 +14,17 @@ RSpec.describe "Vehicles API", type: :request do
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /api/v1/vehicles' do
+    before { get "/api/v1/vehicle/#{vehicle_id}" }
+
+    context 'when the record exists' do
+      it 'returns the vehicle' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(vehicle_id)
+      end
     end
   end
 end
