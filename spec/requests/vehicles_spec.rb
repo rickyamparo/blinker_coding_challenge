67dd3659d1215b12de770940e500921d74279f49
Ricky Amparo <rickyamparo@gmail.com>
2018-02-28 13:09:39 -0700

    add sad path testing for when invalid attributes are passed to post request

--- spec/requests/vehicles_spec.rb
+++ spec/requests/vehicles_spec.rb
@@ -62,5 +62,17 @@ RSpec.describe 'Vehicles API', type: :request do
         expect(response).to have_http_status(201)
       end
     end
+
+    context 'when the record is invalid' do
+      before { post '/api/v1/vehicles', params: { identification: "12345" } }
+
+      it "returns status code 422" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "returns an invalid record message" do
+        expect(response.body).to match(/Validation failed:/)
+      end
+    end
   end
 end
