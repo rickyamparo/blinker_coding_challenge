e8273b01131225bda770773aed1247e26f04e752
Ricky Amparo <rickyamparo@gmail.com>
2018-02-28 15:41:26 -0700

    change the vehicles table migration to remove options array, change them to text instead of string

--- db/migrate/20180227202607_create_vehicles.rb
+++ db/migrate/20180227202607_create_vehicles.rb
@@ -1,9 +1,8 @@
 class CreateVehicles < ActiveRecord::Migration[5.1]
   def change
     create_table :vehicles do |t|
-      t.string :make
-      t.string :model
-      t.text   :options, array: true, default: []
+      t.text :make
+      t.text :model
 
       t.timestamps
     end
