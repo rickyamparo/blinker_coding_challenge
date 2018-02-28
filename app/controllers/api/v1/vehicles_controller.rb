722ecb9e62fe68deca01fa93696459c3e153cd40
Ricky Amparo <rickyamparo@gmail.com>
2018-02-28 10:54:56 -0700

    create strong params in vehicles controller

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -6,4 +6,16 @@ class Api::V1::VehiclesController < ApplicationController
   def show
     json_response(Vehicle.find(params[:id]))
   end
+
+  def create
+    @vehicle = Vehicle.create(vehicle_params)
+    json_response(@vehicle, :created)
+  end
+end
+
+
+private
+
+def vehicle_params
+  params.permit(:id, :make, :model)
 end
