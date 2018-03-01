db8f5940d5f679d44fb1cf4e5e80735a2712da28
Ricky Amparo <rickyamparo@gmail.com>
2018-02-28 21:06:58 -0700

    add routes for creating an association between vehicles and options

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :vehicles
       resources :options
+      resources :vehicles_options, only: [:create, :destroy]
     end
   end
 end
