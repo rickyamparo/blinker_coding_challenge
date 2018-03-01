7e9d90a15b9dc3440a26771490d67d9fa9d11356
Ricky Amparo <rickyamparo@gmail.com>
2018-02-28 20:02:53 -0700

    create post options spec

---
+++ spec/requests/options/post_options_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Options POST API', type: :request do
+
+  describe "POST /api/v1/vehicles" do
+    let(:valid_option_attributes) { { name: 'air conditioning'
+                                   } }
+    context 'when the record is valid' do
+      before { post '/api/v1/options', params: valid_option_attributes}
+
+      it "creates an option" do
+        expect(json['name']).to eq('air conditioning')
+      end
+
+      it "returns status code 201" do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the record is invalid' do
+      before { post '/api/v1/options', params: { title: 'air conditioning' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns an invalid record message' do
+        expect(response.body).to match(/Validation failed:/)
+      end
+    end
+  end
+end
